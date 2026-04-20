6d48b9c1f67a3c71534ba4691b4d8b7535442243
shopdoug8-del <shopdoug8@gmail.com>
2026-04-20 10:38:11 -0300

    Adicionando JavaScript

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  //pegar a tag da imag
+  const img = document.querySelector("#profile img");
+
+  //substituir a image
+  if (html.classList.contains("light")) {
+    //se tiver ligth mode,adicionar a imagem ligth
+    img.setAttribute("src", "../assets/avatar-light.png");
+  } else {
+    //se nao tiver light mode,adicionar a imagem dark
+    img.setAttribute("src", "../assets/avatar.png");
+  }
+}
