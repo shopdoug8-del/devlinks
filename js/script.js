d90e02a6a233e94b72dc0003a609737d5ab85762
shopdoug8-del <shopdoug8@gmail.com>
2026-04-22 08:41:37 -0300

    correção

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   //substituir a image
   if (html.classList.contains("light")) {
     //se tiver ligth mode,adicionar a imagem ligth
-    img.setAttribute("src", "../assets/avatar-light.png");
+    img.setAttribute("src", "./assets/avatar-light.png");
   } else {
     //se nao tiver light mode,adicionar a imagem dark
-    img.setAttribute("src", "../assets/avatar.png");
+    img.setAttribute("src", "./assets/avatar.png");
   }
 }
